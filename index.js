2ea045e7b6a37301f477ba66dec15c8546350f2b
mrahaman1 <mustafijur.rahaman.73586@gmail.com>
2023-05-03 09:33:09 +0530

    b

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ const express = require("express")
 const app = express()
 const PORT = 4005;
 const ApiRoute = require("./Routes/ApiRoute")
-let cors=require("cors")
+const cors=require("cors")
 
 app.use(cors())
-app.use("/",ApiRoute)
+app.use("/", ApiRoute)
 
 app.listen(PORT,()=>{
     console.log("server is running on port",PORT)
