c26c64118baf66b542c314f4a95c9d61bc3a208d
Mustafijur Rahaman <103383337+mrahaman1@users.noreply.github.com>
2023-05-03 08:56:04 +0530

    Update ApiRoute.js

--- Routes/ApiRoute.js
+++ Routes/ApiRoute.js
@@ -2,6 +2,6 @@ const express = require("express")
 const ApiData = require("../Api/ApiData")
 
 const data = express.Router();
-// data.route("/").get(ApiData.apiController)
-data.get("/", ApiData)
+data.route("/").get(ApiData.apiController)
+//data.get("/", ApiData)
 module.exports = data;
